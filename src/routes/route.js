29eac1caf927385dfbbc0f1113b0f0079d7ef1aa
unknown <neerajarao2898@gmail.com>
2022-06-06 22:37:45 +0530

    both post and get api of class assignmments submitted

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,103 @@
-const express = require('express');
-const router = express.Router();
+// Assignment on get api and Post api
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+// Q1:-write an api which gives the missing number in an array of 
+// integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+
+const express = require('express') ;
+const app = express.Router() ;
+const router =express.Router() ;
+
+app.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7];
+    let sum = 0;
+    for (i=0; i<arr.length ; i++){
+        sum = sum+arr[i]
+    }
+    let n= arr.pop();
+    let consecutivesum = n*(n+1)/2;
+    let missingNumber = consecutivesum-sum ;
+
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+});
+ module.exports = app
+
+
+//  **********************************
+// q-2-write an api which gives the missing number
+//  in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 
+// 36 is missing
+app.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let missingNumber =0 ;
+    let n= arr.length;
+    for (i=0; i<arr.length ; i++){
+        if(arr[i+1] - arr[i] >1){
+            missingNumber= arr[i]+1
+        }
+    }
+    res.send({data: missingNumber})
+    ///LOGIC WILL GO HERE 
+
+    res.send(  { data: missingNumber  }  );
+});
+
+// ****************************************************************
+// ASSIGNMENT OF POST API
+// q-3 
+// Write a POST /players api that creates a new player
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+router.post('/players' , function(req,res){
+    let newplayer = req.body
+    let nameunique =true ;
+    for (i=0; i< players.length ; i++){
+        if(newplayer.name == players[i].name){
+            nameunique = false;
+        }
+        if(newplayer.name == ''){
+            nameunique = false
+            res.send('Payer with the name already exist')
+        }
+        if( nameunique = true) {
+            players.push(newplayer);
+
+        }
+        res.send({data : players ,status:  true})
+
+    }
+    
 })
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
-module.exports = router;
+module.exports = router ;
