013946681843d946d43200be982a71c07f2ce20e
unknown <neerajarao2898@gmail.com>
2022-06-16 22:03:29 +0530

    update final

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,18 +4,28 @@ const userModel = require("../models/userModel");
 // ******************QUESTION- 1******************************
 
 const createUser = async function (req, res) {
-  let userData = req.body;
-  let saveduserData = await userModel.create(userData);
-    res.send({ msg: saveduserData });
-};
-// ******************QUESTION- 2******************************
+  try{
+    let userData = req.body;
+    console.log(userData) ;
+     if ( Object.keys(userData).length != 0) {
+      let saveduserData = await userModel.create(userData);
+      res.status(201).send({ msg: saveduserData });
+    }
+    else res.status(400).send({ msg: "BAD REQUEST"})
+    }
+   catch (error) {
+  res.status(500).send({err : error.message})
+}
+}
+  // ******************QUESTION- 2******************************
+  
  const loginuser = async function (req,res){
+  try{
  let emailinfo = req.body.emailId ;
  let passlock = req.body.password ;
  let login = await userModel.findOne({emailId : emailinfo , password :passlock })
-//  res.send({msg : login})
-if (!login)
-  return res.send({status: false ,msg :"Invalid"}) ;
+  if (!login)
+  return  res.status(400).send({status: false ,msg :"Invalid"}) ;
 // token generation
 let token = jwt.sign (
   {
@@ -25,42 +35,56 @@ let token = jwt.sign (
   } , "FunctionUp-Radon"
 ) ;
 res.setHeader("x-auth-token" ,token) 
- return res.send({token: token})
+ return  res.status(200).send({token: token})
+}
+catch(error){
+  res.status(500).send({err : error.message})
+}
  } ;
 
 // ***********************QUESTION- 3****************************
 
 const getUserData = async function (req, res) {
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  // if (!userDetails)
-  //   return res.send({ status: false, msg: "No such user exists" });
-  res.send({ status: true, data: userDetails });
+  try{
+    let userId = req.params.userId;
+    let userDetails = await userModel.findById(userId);
+    res.status(200).send({ status: true, data: userDetails });
+  }
+  catch (error) {
+    res.status(500).send({err : error.message})
+  }
 };
 
 // ************************QUESTION- 4*****************************
 
  const updateUser = async function (req, res) {
-   let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-//   if (!user) 
-//     return res.send("No such user exists");
-   let userData = req.body;
-   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-   res.send({ status: updatedUser, data: updatedUser });
+  try{
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+     let userData = req.body;
+     let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+     res.status(200).send({ status: updatedUser, data: updatedUser });
+  }
+  
+   catch (error) {
+    res.status(500).send({err : error.message})
+  }
 };
 
 // **************************QUESTION- 5********************************
 
 const deleteUser = async function(req,res){
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  //   if (!user) 
-  //     return res.send("No such user exists");
-  let deletedUser =await userModel.findOneAndUpdate(
-              {_id :userId} ,{$set : {isDeleted : true}} ,{new :true}
-              )
-      res.send({deletedUser : deletedUser})
+  try{
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    let deletedUser =await userModel.findOneAndUpdate(
+      {_id :userId} ,{$set : {isDeleted : true}} ,{new :true}
+      )
+      res.status(200).send({deletedUser : deletedUser})
+  }
+  catch (error) {
+        res.status(500).send({err : error.message})
+  }
 } ;
 
 module.exports = {createUser ,getUserData ,updateUser, loginuser , deleteUser}  ;
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,36 +1,45 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 const tokenAuthentication =  function(req,res,next){
-    let token = req.headers["x-Auth-token"];
-    if (!token) 
-    token = req.headers["x-auth-token"];
-    if (!token)
-    return res.send({status :false , msg:"token must be present" }) ;
-    let tokenDecoded = jwt.verify(token , "FunctionUp-Radon" )
-    if(!tokenDecoded)
-    return res.send({status :false , msg : "Invalid Token"})
+    try{
+        let token = req.headers["x-Auth-token"];
+        if (!token) 
+        token = req.headers["x-auth-token"];
+        if (!token)
+        return res.status(401).send({status :false , msg:"token must be present" }) ;
+        let tokenDecoded = jwt.verify(token , "FunctionUp-Radon" )
+        if(!tokenDecoded)
+        return res.status(401).send({status :false , msg : "Invalid Token"})
+    }
+    catch (error) {
+        res.status(500).send({err : error.message})
+      }
     next()
 }
 
 const tokenAuthorisation =  async function(req, res, next){
-    let token = req.headers["x-Auth-token"];
+    try{
+        let token = req.headers["x-Auth-token"];
     if (!token) 
     token = req.headers["x-auth-token"];
 
     let tokenDecoded = jwt.verify(token , "FunctionUp-Radon" )
 
-    // let userId = req.params.userId;
     let userToBeModified = req.params.userId
     let userLoggedIn = tokenDecoded.userId
 
     let user = await userModel.findById(userToBeModified);
     if (!user) {
-      return res.send("No such user exists");
+      return res.status(400).send("No such user exists");
     }
     if(userToBeModified != userLoggedIn) {
-        return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+        return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
     }
-    next()
+    }
     
+    catch (error) {
+        res.status(500).send({err : error.message})
+      }
+    next()
 }
 module.exports = {tokenAuthentication , tokenAuthorisation } 
